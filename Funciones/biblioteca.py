6cf902b0301ce3ec185bf4e863965057bcbe980d
WilliamTMC <willitotcm2006@gmail.com>
2025-11-26 04:44:29 +0100

    Termine las mipymes y adelante parte del analisis

--- Funciones/biblioteca.py
+++ Funciones/biblioteca.py
@@ -3,3 +3,12 @@ def open_json(path):
     with open(path,"r",encoding="utf-8") as file :
         read = json.loads(file.read())
     return read
+def suma(l:list):
+    suma = 0
+    for i in l:
+        suma += i 
+    return suma
+def promedio(l:list):
+    s = suma(l)
+    p = s/len(l)
+    return p
