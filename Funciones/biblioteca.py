c8c7a97dba2c204569996280fe76064923d5821d
WilliamTMC <willitotcm2006@gmail.com>
2025-12-04 16:46:32 +0100

    fixes

--- Funciones/biblioteca.py
+++ Funciones/biblioteca.py
@@ -3,16 +3,25 @@ def open_json(path):
     with open(path,"r",encoding="utf-8") as file :
         read = json.loads(file.read())
     return read
+
 def suma(l:list):
     suma = 0
     for i in l:
         suma += i 
     return suma
+
 def promedio(l:list):
     s = suma(l)
     p = s/len(l)
     return p
+
 def convertir(n:int,m:int):
     con = n * m 
-    return con 
-def comparar(a,b,c):
+    return round(con)
+
+def valores_repetidos(l:list):
+    repetidos = []
+    for elemento in l:
+        if l.count(elemento) > 1 and elemento not in repetidos:
+            repetidos.append(elemento)
+    return repetidos
